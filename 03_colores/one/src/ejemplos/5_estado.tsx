890305a12f54edf5087fbededb5181335e6cf3dc
Marco Rossel <andresmarcorossel@gmail.com>
2025-11-27 15:35:43 -0300

    tailwind | mathML market language

--- 03_colores/one/src/ejemplos/5_estado.tsx
+++ 03_colores/one/src/ejemplos/5_estado.tsx
@@ -1,7 +1,13 @@
 export default function Estados() {
   return (
-    <div>
-      <p>Estados</p>
+    <div className="bg-blue-500 p-1.5 max-w-3xl mx-auto ">
+      <form className="">
+        <label htmlFor="name">name</label>
+        <input type="text" name="name" />
+        <label htmlFor="email">email</label>
+        <input type="email" name="email" id="email" />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
