c9c1a82b3631f860c13da717c17cc4071c8431aa
Marco Rossel <andresmarcorossel@gmail.com>
2025-12-04 02:05:37 -0300

    tailwind | challenge frontend 2 | font problem

---
+++ 04_desafios_frontend/2_challenge/src/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <p className="text-red-500">Testingi tailwind css</p>
+      <div className="font-dm italic font-medium">
+        <p>some test or the font</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat
+          dolores veniam necessitatibus incidunt! Repellendus atque voluptates
+          exercitationem doloremque accusamus! Nobis ducimus quos minus ad
+          pariatur necessitatibus, beatae quisquam possimus. Impedit fugit
+          explicabo, unde vero debitis sapiente ab odio. Libero debitis
+          doloremque voluptatibus quae voluptatum reprehenderit neque optio,
+          velit ad similique. Ullam ea, minima eaque neque provident accusamus
+          mollitia qui?
+        </p>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repellendus doloribus quas necessitatibus praesentium? Totam ullam
+            repellendus vel sit eum similique porro? Quasi recusandae suscipit
+            ratione soluta officia tempore voluptate rem.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
