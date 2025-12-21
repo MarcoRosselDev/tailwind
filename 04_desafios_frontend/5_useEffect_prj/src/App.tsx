dacb66afd23eade056e2f29d1b82ba82e579b29b
Marco Rossel <andresmarcorossel@gmail.com>
2025-12-21 12:33:20 -0300

    tailwind | challenge frontend 2 weather app | useEffect and custom hook profunditation

--- 04_desafios_frontend/5_useEffect_prj/src/App.tsx
+++ 04_desafios_frontend/5_useEffect_prj/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import useFetch from "./hook/useFetch";
 
 type FakeApi = {
   userId: number;
@@ -6,35 +6,11 @@ type FakeApi = {
   title: string;
   completed: boolean;
 };
-function App() {
-  const [data, setData] = useState<FakeApi | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [err_message, setErr_message] = useState<null | string>(null);
 
-  useEffect(() => {
-    fetch("https://alskjdfasdf.com")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Error en la API, o error controlado");
-      })
-      .then((res_json: FakeApi) => {
-        setData(res_json);
-        return;
-      })
-      .catch((err) => {
-        if (err instanceof Error) {
-          console.log({ message: err.message });
-          setErr_message(err.message);
-          return;
-        } else {
-          console.log("error mas grave");
-          setErr_message("error mas grave");
-        }
-      })
-      .finally(() => setIsLoading(false));
-  }, []);
+function App() {
+  const { data, err_message, isLoading } = useFetch<FakeApi>(
+    "https://jsonplaceholder.typicode.com/todos/4"
+  );
 
   if (err_message) return <p>error : {err_message}</p>;
   if (isLoading || !data) return <p>cargando...</p>;
@@ -53,35 +29,3 @@ function App() {
 }
 
 export default App;
-
-/* 
-const [menu, setMenu] = useState(false);
-  const [data, setData] = useState<null | FakeApi>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errorState, setErrorState] = useState<boolean | string>(false);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/4")
-      .then((response) => response.json())
-      .then((json) => {
-        if (json.ok) setData(json);
-        throw new Error("error en el servidor");
-      })
-      .catch((e) => {
-        if (e instanceof Error) {
-          setErrorState(e.message);
-          return;
-        }
-        setErrorState("error mas grave");
-        console.log("error mas grave");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) return <p>Is Loading ...</p>;
-  if (errorState) return <p>error : {errorState}</p>;
-
-  if (!data) return <p>no data amiguito</p>;
-*/
