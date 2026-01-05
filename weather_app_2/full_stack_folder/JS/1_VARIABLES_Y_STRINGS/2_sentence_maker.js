b9f4245c61a916ed51e5e771653dc5114c914d21
Marco Rossel <andresmarcorossel@gmail.com>
2026-01-05 04:38:53 -0300

    tailwind | challenge frontend 2 weather app | js strings and variables

---
+++ weather_app_2/full_stack_folder/JS/1_VARIABLES_Y_STRINGS/2_sentence_maker.js
@@ -0,0 +1,21 @@
+let adjective = "tall";
+let noun = "book";
+let verb = "go";
+let place = "house";
+let adjective2 = "short";
+let noun2 = "pencil";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+adjective = "ugly";
+noun = "person";
+verb = "run";
+place = "garden";
+adjective2 = "beautiful";
+noun2 = "dog";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
