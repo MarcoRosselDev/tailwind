9ed62e79b871732789b7b534a1121e7e53fc5d79
Marco Rossel <andresmarcorossel@gmail.com>
2026-02-07 05:29:02 -0300

    tailwind | challenge frontend 6 | drag and drop librery, esta bien enredada la cosa

---
+++ 04_desafios_frontend/9_testing_drag_and_drop/src/version_1/Example.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import {DndContext} from '@dnd-kit/core';
+import {Draggable} from './Draggable'
+import {Droppable} from './Droppable'
+
+function Example() {
+  const [parent, setParent] = useState(null);
+  const draggable = (
+    <Draggable id="draggable" className="drop">
+      Go ahead, drag me.
+    </Draggable>
+  );
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!parent ? draggable : null}
+      <Droppable id="droppable">
+        {parent === "droppable" ? draggable : 'Drop here'}
+      </Droppable>
+    </DndContext>
+  );
+
+  function handleDragEnd({over}) {
+    setParent(over ? over.id : null);
+  }
+}
+
+export default Example
