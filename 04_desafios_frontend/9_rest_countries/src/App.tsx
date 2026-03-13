cd57d6188b670080fd3a92162508463fb8c4260b
Marco Rossel <andresmarcorossel@gmail.com>
2026-03-13 18:49:35 -0300

    font custom

--- 04_desafios_frontend/9_rest_countries/src/App.tsx
+++ 04_desafios_frontend/9_rest_countries/src/App.tsx
@@ -1,7 +1,27 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <div className="b-2">
-      <p className="text-3xl text-red-600">testing tailwind</p>
+    <div className="b-2 dark:bg-gray-800 dark:text-white  min-h-dvh font-nunito-variable">
+      <Header />
+      <div>
+        <form >
+          <input type="text" 
+          placeholder="Search for a country..." 
+          className="p-4 border w-full"
+          />
+        </form>
+        <div>
+          <h1>Filter by Region</h1>
+          <ul>
+            <li>Africa</li>
+            <li>America</li>
+            <li>Asia</li>
+            <li>Europa</li>
+            <li>Oceania</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
