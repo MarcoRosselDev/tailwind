df88a8679045da2adc385106b857e0491df1018a
Marco Rossel <andresmarcorossel@gmail.com>
2026-03-16 23:15:43 -0300

    solving some problem from python algorithm course, just now

---
+++ 04_desafios_frontend/9_rest_countries/py/2_strings.py
@@ -0,0 +1,11 @@
+str = "marco rossel"
+
+print(str[0:2]) # ma
+print(str[0:3]) # mar
+print(str[3:5]) # co
+
+# in como operador logico
+
+print("mar" in str) # true
+print("ros" in str) # true
+print("lush" in str) # false
