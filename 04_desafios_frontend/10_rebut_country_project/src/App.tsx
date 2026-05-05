a82e9d186ddf01f9a4f84a017672bfb13b7a029b
Marco Rossel <andresmarcorossel@gmail.com>
2026-05-05 16:32:56 -0400

    reboot

--- 04_desafios_frontend/10_rebut_country_project/src/App.tsx
+++ 04_desafios_frontend/10_rebut_country_project/src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css'
-import Header from "./components/Header.jsx"
+import Header from "./components/Header.js"
 
 function App() {
 
   return (
-    <>
+
+    <div className='bg-white dark:bg-black'>
       <Header />
       <p className='text-red-600'>testing</p>
       <div>
         <p>testing the main content</p>
       </div>
-    </>
+    </div>
   )
 }
 
