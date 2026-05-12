f18bb321ce5cb7fdc2f7e04b276160c13ba73ae6
Marco Rossel <andresmarcorossel@gmail.com>
2026-05-12 06:18:10 -0400

    html and py course

--- 04_desafios_frontend/10_rebut_country_project/src/App.tsx
+++ 04_desafios_frontend/10_rebut_country_project/src/App.tsx
@@ -1,15 +1,26 @@
 import './App.css'
 import Header from "./components/Header.js"
+import data from "./data.json"
 
 function App() {
 
+  console.log(data[249]);
+
+
   return (
 
-    <div className='bg-white dark:bg-black'>
+    <div className='bg-white dark:bg-black dark:text-white'>
       <Header />
-      <p className='text-red-600'>testing</p>
+      <p className='text-red-600 '>testing</p>
+      <form>
+        <input type="text" placeholder='Search for a country...' />
+
+      </form>
       <div>
-        <p>testing the main content</p>
+        <p>this gona be a filter</p>
+      </div>
+      <div>
+        <p>this gona be all the countrys</p>
       </div>
     </div>
   )
