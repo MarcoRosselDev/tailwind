284ea86bf8f7cdcd422737657674ab618975eb89
Marco Rossel <andresmarcorossel@gmail.com>
2026-05-27 19:51:35 -0400

    html and py course | 1 forms

--- 04_desafios_frontend/10_rebut_country_project/py_course/5_functions_and_scope/2_lab.py
+++ 04_desafios_frontend/10_rebut_country_project/py_course/5_functions_and_scope/2_lab.py
@@ -1,9 +1,25 @@
-def caesar():
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    shift = 5
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    translation_table = str.maketrans(alphabet, shifted_alphabet)
-    text = 'hello world'
-    encrypted_text = text.translate(translation_table)
-    print(encrypted_text)
+def caesar(text, shift, encrypt=True):
+    if not isinstance(shift, int):
+        return "Shift must be an integer value."
     
+    if shift < 1 or shift > 25:
+        return "Shift must be an integer between 1 and 25."
+
+    if not encrypt:
+        shift = -shift
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    return text.translate(translation_table)
+
+def decrypt(text, shift):
+    return caesar(text, shift, False)
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+
+encrypted_text = "Pbhentr vf sbhaq va hayvxryl cynprf."
+decrypted_text = decrypt(encrypted_text, 13)
+
+print(decrypted_text)
--- 04_desafios_frontend/10_rebut_country_project/py_course/5_functions_and_scope/5.py
+++ 04_desafios_frontend/10_rebut_country_project/py_course/5_functions_and_scope/5.py
@@ -1,2 +1,29 @@
-name = input("Cual es tu nombre?")
-print(f"Hola {name}, buen dia!")
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(char, strength, intelligence, charisma):
+  is_string = isinstance(char, str)
+  strength_int = isinstance(strength, int)
+  intelligence_int = isinstance(intelligence, int)
+  charisma_int = isinstance(charisma, int)
+
+  if not is_string:
+    return "The character name should be a string"
+  elif len(char) == 0:
+    return "The character should have a name"
+  elif len(char) > 10:
+    return "The character name is too long"
+  elif " " in char:
+    return "The character name should not contain spaces"
+  elif not strength_int or not intelligence_int or not charisma_int:
+    return "All stats should be integers"
+  elif strength < 1 or intelligence < 1 or charisma < 1 :
+    return "All stats should be no less than 1"
+  elif strength > 4 or intelligence > 4 or charisma > 4 :
+    return "All stats should be no more than 4"
+  elif strength + intelligence + charisma != 7:
+    return "The character should start with 7 points"
+  else:
+    return f"{char}\nSTR {strength*full_dot + (10-strength)*empty_dot}\nINT {intelligence*full_dot + (10-intelligence)*empty_dot}\nCHA {charisma*full_dot + (10-charisma)*empty_dot}"
+
+  
